d275e80ad2e4bd1cccd145f538d3e35fbfbf824d
iffertmedia <69326091+iffertmedia@users.noreply.github.com>
2025-07-30 21:23:30 -0500

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';import { Routes, Route } from 'react-router-dom';
-    export default function App() {
-      return (
-        <Routes>
-          <Route path="/" element={<div className="text-2xl font-bold p-4">
-          Welcome to ShopLink Web Dashboard</div>} />
-        </Routes>
-      );
-    }
